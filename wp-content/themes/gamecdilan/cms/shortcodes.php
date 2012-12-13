fa67ef78170d595edeb5017226833ad3bdd0389c
cdilan <none@none.com>
2012-12-13 10:45:58 -0200

    refaz o shortcode de conexão com redes sociais
    tira o lanoba e coloca o social connect

--- wp-content/themes/gamecdilan/cms/shortcodes.php
+++ wp-content/themes/gamecdilan/cms/shortcodes.php
@@ -1,38 +1,12 @@
 <?php
 
-/***************************************************
-# Lanoba
-**************************************************
-
-add_shortcode( 'lanoba', 'lb_shortcode' );
-function lb_shortcode () {
-    if ( is_user_logged_in() ) {
-    	global $current_user;
-    	$id = $current_user->id;
-    	$display_name = $current_user->display_name;
-    	$saida = '	<div id="lanoba" class="logado">
-	    				<div class="profile_user">
-							<div class="profile_avatar">
-								'.get_avatar($id,64).'
-							</div>    				
-	    					<div class="profile_display_name">
-	    						'.$display_name.'
-	    					</div>
-	    				</div>
-	    				<div class="profile_logout">
-							<a href="'.wp_logout_url('perfil').'" title="Logout">Logout</a>
-		    			</div>
-	    			</div>';
-    	return ($saida);
-    }else{
-    	$iframe = (lb_LoginWidget('',0,0,0));
-    	$saida = '<div id="lanoba" class="deslogado" >'.$iframe.'</div>';
-    	return ($saida);
-    }
-}*/
-
-add_shortcode( 'lanoba', 'lb_shortcode' );
-	function lb_shortcode () {
+//	##################################
+	##          sconnect 			##
+	## como usar:					##
+	## 		[sconnect]				##
+	##################################
+add_shortcode( 'sconnect', 'sconnect_shortcode' );
+	function sconnect_shortcode () {
 		do_action( 'social_connect_form' );
 	}
 
