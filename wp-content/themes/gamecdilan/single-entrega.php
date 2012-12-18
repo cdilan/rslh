ac2888759d46ddcbb2b523e0f1980475e9d533f9
Mahmoud Aoki <mahaoki@gmail.com>
2012-12-18 19:10:50 -0200

    Insere botão de fazer atividade no detalhe da entrega

--- wp-content/themes/gamecdilan/single-entrega.php
+++ wp-content/themes/gamecdilan/single-entrega.php
@@ -5,25 +5,14 @@
                     
                     <section>
                         <div class="container">
-                            <div class="page-header">
-                                <h1><?php the_title(); ?></h1>
-                            </div>
                             <div class="row">
                                 <div class="span8">
+                                    <div class="page-header">
+                                        <h1><?php the_title(); ?></h1>
+                                    </div>
                                     <div id="entrega" class="thumbnail entry">
                                         <?php the_content(); ?>
                                     </div>
-                                    <section id="comentarios">
-                                        <?php comments_template( '', true ); ?>
-                                    </section>                   
-                                </div>
-                                <aside class="span4">
-                                    <div id="entrega-jogador" class="well well-small">
-                                        <?php echo get_avatar( get_the_author_meta('ID'), 80 ); ?>
-                                        <h3><?php the_author(); ?></h3>
-                                        <h4><?php echo get_user_meta(get_the_author_meta('ID'),'lanhouse', true); ?></h4>
-                                    </div>
-
                                     <?php
                                         global $post;
                                         $tmp_post = $post;
@@ -33,21 +22,20 @@
                                         $args = array(  'numberposts' => 20,
                                                         'post_type'=>'entrega',
                                                         'orderby'=>'rand',
-                                                        'exclude' => $post->ID,
                                                         'tax_query'=> array ( array (   'taxonomy'=>'tag_atividade',
                                                                                         'field'=>'id',
                                                                                         'terms'=>$tag_atividade_id[0] ))) ;
                                         $myposts = get_posts( $args );
                                         if(!empty($myposts)) :
                                     ?>
-                                        <section id="entrega-jogadores">
-                                            <h3>Jogadores que também fizeram esta atividade</h3>
+                                        <section id="lista-entregas">
+                                            <h3>Jogadores que entregaram essa atividade</h3>
                                             <ul class="thumbnails">
                                                 <?php
                                                 foreach( $myposts as $post ) :  setup_postdata($post); ?>
-                                                    <li class="span4">
+                                                    <li class="span2">
                                                         <div class="thumbnail">
-                                                            <a href="<?php the_permalink(); ?>"><?php echo get_avatar( get_the_author_meta('ID'), 48 ); ?></a>
+                                                            <a href="<?php the_permalink(); ?>"><?php echo get_avatar( get_the_author_meta('ID'), 120 ); ?></a>
                                                             <h5><a href="<?php the_permalink(); ?>"><?php the_author(); ?></a></h5>
                                                         </div>
                                                     </li>
@@ -56,6 +44,40 @@
                                         </section>
                                     <?php endif; ?>
                                     <?php $post = $tmp_post; ?>
+                                    <section id="comentarios">
+                                        <?php comments_template( '', true ); ?>
+                                    </section>
+                                </div>
+                                <aside class="span4">
+                                    <div id="btn-atividade">
+                                    <?php
+                                        global $post;
+                                        $tmp_post = $post;
+                                        // pega tag_atividade deste post
+                                        $tag_atividade_id = (wp_get_post_terms($post->ID, 'tag_atividade', array("fields" => "ids")));
+                                        //lista entregas com essa tag_atividade
+                                        $args = array(  'numberposts' => 1,
+                                                        'post_type'=>'atividade',
+                                                        'tax_query'=> array ( array (   'taxonomy'=>'tag_atividade',
+                                                                                        'field'=>'id',
+                                                                                        'terms'=>$tag_atividade_id[0] ))) ;
+                                        $myposts = get_posts( $args );
+                                        if(!empty($myposts)) :
+                                            foreach( $myposts as $post ) :  setup_postdata($post);
+                                    ?>
+                                        <a class="btn btn-large" href="<?php the_permalink(); ?>">Faça essa atividades</a>
+
+                                    <?php
+                                        endforeach;
+                                        endif; 
+                                        $post = $tmp_post;
+                                    ?>
+                                    </div>
+                                    <div id="entrega-jogador" class="well well-small">
+                                        <?php echo get_avatar( get_the_author_meta('ID'), 80 ); ?>
+                                        <h3><?php the_author(); ?></h3>
+                                        <h4><?php echo get_user_meta(get_the_author_meta('ID'),'lanhouse', true); ?></h4>
+                                    </div>
                                 </aside>
                             </div>
                         </div>
