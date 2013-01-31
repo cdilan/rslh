5f9815dac9b000f4ecd485bb8457f7466bc1ea06
Francisco WordPress <fmatelli@gmail.com>
2013-01-31 14:15:22 -0200

    Adiciona cor e ícone na lista de atividades
    Para o jogador saber qual visitou e completou, além de adicionar outra
    cor na barra de progresso

--- wp-content/themes/gamecdilan/taxonomy-episodio.php
+++ wp-content/themes/gamecdilan/taxonomy-episodio.php
@@ -19,12 +19,25 @@
                             <ul class="thumbnails">
                                 <?php if (have_posts()) : ?>
                                     <?php while (have_posts()) : the_post(); ?>
-                                        <li class="span6">
-                                            <div class="thumbnail">
+                                    <?php $status_atividade = get_user_meta(get_current_user_id(),$post->ID, true); ?>
+                                    <?php if($status_atividade=="visited") {$bgcor="#E6FFCC";} elseif ($status_atividade=="completed") {$bgcor="#FFDBB8";} else {$bgcor="transparent";} ?>
+                                        <li class="span6 ">
+                                            <div class="thumbnail" style="background:<?php echo $bgcor; ?>">
                                                 <?php if (has_post_thumbnail()) { the_post_thumbnail('thumbnail'); } ?>
+                                                
                                                 <h2><?php the_title(); ?></h2>
                                                 <div><?php echo substr(get_the_excerpt(), 0, 180); ?>...</div>
                                                 <a href="<?php the_permalink(); ?>" class="btn btn-primary">Fazer atividade</a>
+                                                <?php 
+                                                //icon-check icon-eye-open
+                                                //PEGADA - Sistema para salvar o rastro, regasta informação no banco de dados se usuário já visitou página 
+                                                if($status_atividade=="visited") {
+                                                    echo '<a class="btn pull-right" href="#"><i class="icon-eye-open"></i> visitada</a>';
+                                                } elseif($status_atividade=="completed") {
+                                                    echo '<a class="btn pull-right" href="#"><i class="icon-check"></i> completada</a>';
+                                                }
+
+                                                ?>
                                             </div>
                                         </li>
                                     <?php endwhile; ?>
